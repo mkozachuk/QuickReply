e61fd2b9976fe3efdd7ed93505dcfa784088f6f0
Maksym <maksym.kozachuk@protonmail.com>
2020-06-12 21:20:22 +0200

    initial

---
+++ src/main/java/pl/devodds/mkozachuk/quickreply/QuickReplyApplication.java
@@ -0,0 +1,13 @@
+package pl.devodds.mkozachuk.quickreply;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuickReplyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QuickReplyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/devodds/mkozachuk/quickreply/QuickReplyApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.devodds.mkozachuk.quickreply;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuickReplyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
